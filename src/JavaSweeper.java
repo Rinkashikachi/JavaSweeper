95be996111b33f0bf44daa2748749b79c7db10b9
Rinkashikachi <15rinkashikachi15@gmail.com>
2018-11-12 12:14:31 +0300

    Creation of window.

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,2 +1,33 @@
-public class JavaSweeper {
+import javax.swing.*;
+import java.awt.*;
+
+public class JavaSweeper extends JFrame{
+
+    private JPanel panel;
+    private final int COLUMNS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
+
+    public static void main(String[] args){
+        new JavaSweeper().setVisible(true);
+    }
+
+    private JavaSweeper(){
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel(){
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(COLUMNS*IMAGE_SIZE,ROWS*IMAGE_SIZE));
+        add(panel);
+    }
+
+    private void initFrame(){
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("JavaSweeper");
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
 }
